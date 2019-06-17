0da6080f77f2ebfd377e6a5fc10bcb5b3322ae73
Emrullah Ayın <ayinemrullah@gmail.com>
2019-06-17 11:39:25 +0300

    import redux

--- src/index.js
+++ src/index.js
@@ -2,4 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./reducers/rootReducer";
+
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+  counter: 0
+};
+
+function rootReducer(state = initialState, action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return { counter: state.counter + 1 };
+    case "DECREMENT":
+      return { counter: state.counter - 1 };
+    default:
+      return state;
+  }
+}
